2ac2a0c1a5bd7f91f32eb2d98af862a40d8f0a38
Афанасьев Александр <afanasjev@mts.ru>
2025-12-09 14:30:04 +0300

    new iter3

--- internal/repository/memstorage/memstorage.go
+++ internal/repository/memstorage/memstorage.go
@@ -40,13 +40,8 @@ func GetMemStorage() *MemStorage {
 }
 
 func (ms *MemStorage) SetCounter(name string, value int64) error {
-	if _, ok := ms.counter.data[name]; ok {
-		ms.counter.mutex.Lock()
-		ms.counter.data[name] += value
-		ms.counter.mutex.Unlock()
-		return nil
-	}
-
+	ms.counter.mutex.Lock()
+	defer ms.counter.mutex.Unlock()
 	ms.counter.data[name] += value
 	return nil
 }
@@ -56,17 +51,26 @@ func (ms *MemStorage) PrintCounter() string {
 }
 
 func (ms *MemStorage) SetGauge(name string, value float64) error {
-	if _, ok := ms.gauge.data[name]; ok {
-		ms.gauge.mutex.Lock()
-		ms.gauge.data[name] = value
-		ms.gauge.mutex.Unlock()
-		return nil
-	}
-
+	ms.gauge.mutex.Lock()
+	defer ms.gauge.mutex.Unlock()
 	ms.gauge.data[name] = value
 	return nil
 }
 
 func (ms *MemStorage) PrintGauge() string {
 	return fmt.Sprintf("%#v", storage.gauge.data)
+}
+
+func (ms *MemStorage) GetCounter(name string) (int64, bool) {
+	ms.counter.mutex.Lock()
+	defer ms.counter.mutex.Unlock()
+	value, ok := ms.counter.data[name]
+	return value, ok
+}
+
+func (ms *MemStorage) GetGauge(name string) (float64, bool) {
+	ms.gauge.mutex.Lock()
+	defer ms.gauge.mutex.Unlock()
+	value, ok := ms.gauge.data[name]
+	return value, ok
 }
